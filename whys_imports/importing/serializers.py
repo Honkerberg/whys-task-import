08415b47452531310ac6018741d0f607480ee8f8
Martin Ošťádal <ostadalmartin96@gmail.com>
2024-02-28 16:25:07 +0100

    refactor(importing-serializers): :recycle: serializers refactored
    All serializers for all models created, even for Catalog model

--- whys_imports/importing/serializers.py
+++ whys_imports/importing/serializers.py
@@ -13,9 +13,10 @@ from .models import (
 
 
 class AttributeNameSerializer(serializers.ModelSerializer):
+    nazev = serializers.CharField(required=False)
     class Meta:
         model = AttributeName
-        fields = "__all__"
+        fields = ['nazev', 'kod', 'zobrazit']
 
 
 class AttributeValueSerializer(serializers.ModelSerializer):
@@ -27,7 +28,12 @@ class AttributeValueSerializer(serializers.ModelSerializer):
 class AttributeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attribute
-        fields = "__all__"
+        fields = ["nazev_atributu_id", "hodnota_atributu_id"]
+
+    def to_representation(self, instance):
+        self.fields["nazev_atributu_id"] = AttributeNameSerializer(read_only=True)
+        self.fields["hodnota_atributu_id"] = AttributeValueSerializer(read_only=True)
+        return super(AttributeSerializer, self).to_representation(instance)
 
 
 class ImageSerializer(serializers.ModelSerializer):
@@ -47,14 +53,41 @@ class ProductAttributesSerializer(serializers.ModelSerializer):
         model = ProductAttributes
         fields = "__all__"
 
+    def to_representation(self, instance):
+        self.fields['attribute'] = AttributeSerializer(read_only=True)
+        return super(ProductAttributesSerializer, self).to_representation(instance)
+
 
 class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImage
         fields = "__all__"
 
+    def to_representation(self, instance):
+        self.fields['product'] = ProductSerializer(read_only=True)
+        self.fields['obrazek_id'] = ImageSerializer(read_only=True)
+        return super(ProductImageSerializer, self).to_representation(instance)
+
 
 class CatalogSerializer(serializers.ModelSerializer):
+    products_ids = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all(), many=True, required=False)
+    attributes_ids = serializers.PrimaryKeyRelatedField(queryset=Attribute.objects.all(), many=True, required=False)
+
     class Meta:
         model = Catalog
-        fields = "__all__"
+        fields = ['nazev', 'obrazek_id', 'products_ids', 'attributes_ids']
+
+    def create(self, validated_data):
+        products_data = validated_data.pop('products_ids', [])
+        attributes_data = validated_data.pop('attributes_ids', [])
+
+        catalog = Catalog.objects.create(**validated_data)
+
+        catalog.products_ids.set(products_data)
+        catalog.attributes_ids.set(attributes_data)
+
+        return catalog
+
+    def to_representation(self, instance):
+        self.fields['obrazek_id'] = ImageSerializer(read_only=True)
+        return super().to_representation(instance)
