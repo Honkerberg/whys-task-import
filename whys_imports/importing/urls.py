1764a17dbde09648e575d29276273028de8ccef2
Martin Ošťádal <ostadalmartin96@gmail.com>
2024-02-28 16:25:55 +0100

    feat(importing-urls): :sparkles: Urls created according to task
    Routers implemented with prefix

--- whys_imports/importing/urls.py
+++ whys_imports/importing/urls.py
@@ -1,7 +1,39 @@
+from django.conf.urls import include
 from django.urls import path
-from . import views
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    AttributeNameViewSet,
+    AttributeValueViewSet,
+    AttributeViewSet,
+    CatalogViewSet,
+    ImageViewSet,
+    ImportViewSet,
+    ProductAttributeViewSet,
+    ProductImageViewSet,
+    ProductViewSet,
+)
+
+router = DefaultRouter()
+router_import = DefaultRouter()
+
+router_import.register("import", ImportViewSet, basename="import")
+router.register("attributename", AttributeNameViewSet)
+router.register("attribute", AttributeViewSet)
+router.register("attributevalue", AttributeValueViewSet)
+router.register("catalog", CatalogViewSet)
+router.register("image", ImageViewSet)
+router.register("product", ProductViewSet)
+router.register("productattribute", ProductAttributeViewSet)
+router.register("productimage", ProductImageViewSet)
 
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", include(router.urls)),
+    path("", include(router_import.urls)),
+]
+
+urlpatterns = [
+    path(r"detail/", include(router.urls)),
+    path(r"", include(router_import.urls)),
 ]
